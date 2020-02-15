6e264df30e69ae101934c99fd6ec6d58eecb51d7
cltq <541989785@qq.com>
2020-02-15 11:45:10 +0800

    已添加        src/main/java/com/hujiang/project/zhgd/sbGroup/domain/CompanyLibrary.java 已添加        src/main/java/com/hujiang/project/zhgd/sbGroup/domain/ProjectList.java 已添加        src/main/java/com/hujiang/project/zhgd/sbGroup/domain/TotalList.java 已添加        src/main/resources/application.yml

---
+++ src/main/java/com/hujiang/project/zhgd/sbGroup/domain/CompanyLibrary.java
@@ -0,0 +1,25 @@
+package com.hujiang.project.zhgd.sbGroup.domain;
+
+public class CompanyLibrary
+{
+	/** 所在城市 */
+	private String city;
+	/**集团名称 */
+	private String name;
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/main/java/com/hujiang/project/zhgd/sbGroup/domain/ProjectList.java
@@ -0,0 +1,35 @@
+package com.hujiang.project.zhgd.sbGroup.domain;
+
+public class ProjectList
+{
+	/** 经度 */
+	private String longitude;
+	/**纬度 */
+	private String latitude;
+	/**项目名称 */
+	private String name;
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
