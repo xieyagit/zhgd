2eb799dec077d5e0e18a10c15faf7257816592c7
cltq <541989785@qq.com>
2020-04-16 12:00:55 +0800

    已修改        src/main/java/com/hujiang/common/utils/FaceMatchUtil.java

--- src/main/java/com/hujiang/common/utils/FaceMatchUtil.java
+++ src/main/java/com/hujiang/common/utils/FaceMatchUtil.java
@@ -5,20 +5,23 @@ import com.baidu.aip.face.AipFace;
 import com.baidu.aip.face.MatchRequest;
 import com.hujiang.project.zhgd.utils.Constants;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FaceMatchUtil {
 
-
+    private static Logger logger = LoggerFactory.getLogger(FaceMatchUtil.class);
     /**
      * 百度人脸对比
+     *
      * @param url1 照片url
      * @param url2 照片url
      * @return
      */
-    public static Integer contrast(String url1, String url2){
+    public static Integer contrast(String url1, String url2) {
 
         Integer number = 1;
         Integer num = 0;
@@ -26,16 +29,16 @@ public class FaceMatchUtil {
         List<MatchRequest> input = new ArrayList<>();
         input.add(new MatchRequest(url1, "URL"));
         input.add(new MatchRequest(url2, "URL"));
+        logger.error("\r\nIOS FACE========>" + url2);
         JSONObject result = aipFace.match(input);
         if (result.getString("error_msg").equals("SUCCESS")) {
             Double score = result.getJSONObject("result").getDouble("score");
             if (score >= Constants.FACESCORE) {
-                   return number;
+                return number;
             }
         }
         return num;
     }
-
 
     public static void deleteUrl(String url) {
         OSSClient ossClient = AliyunOSSClientUtil.getOSSClient();
@@ -48,7 +51,5 @@ public class FaceMatchUtil {
         }
         AliyunOSSClientUtil.deleteFile(ossClient, "hujiang", folder, file);
     }
-
-
 
 }
